f02566575aa88055d1f4f5a73dd53814879ae7ae
Alex Lalejini <amlalejini@gmail.com>
2021-07-30 14:22:24 -0400

    add test for bouncing balls

--- tests/problems.cpp
+++ tests/problems.cpp
@@ -13,7 +13,7 @@ TEST_CASE("Basement", "[problem][psb2]") {
   const std::string data_path("problem-data/basement/basement.json");
 
   problem_t problem;
-  REQUIRE(problem.name() == "Basement");
+  REQUIRE(problem.name() == "basement");
 
   SECTION("LOAD TESTS FROM JSON") {
     emp::vector<
@@ -39,5 +39,49 @@ TEST_CASE("Basement", "[problem][psb2]") {
       3
     };
     REQUIRE(loaded_tests[0]==correct);
+  }
+}
+
+TEST_CASE("BouncingBalls", "[problem][psb2]") {
+  using problem_t = psb::BouncingBalls;
+  const std::string data_path("problem-data/bouncing-balls/bouncing-balls.json");
+
+  problem_t problem;
+  REQUIRE(problem.name() == "bouncing-balls");
+
+  SECTION("LOAD TESTS FROM JSON") {
+    emp::vector<
+      std::pair<problem_t::input_t, problem_t::output_t>
+    > loaded_tests;
+
+    std::ifstream data_ifstream(data_path);
+    nlohmann::json data_json;
+
+    std::string line;
+    while (std::getline(data_ifstream, line)) {
+      loaded_tests.emplace_back(problem.ReadTestJSON(line));
+    }
+    data_ifstream.close();
+
+    REQUIRE(loaded_tests.size() == 3);
+
+    std::pair<
+      problem_t::input_t,
+      problem_t::output_t
+    > correct = {
+      std::make_tuple(1.001, 1.0, 1),
+      2.001
+    };
+    REQUIRE(loaded_tests[0]==correct);
+
+    // for (size_t i = 0; i < loaded_tests.size(); ++i) {
+    //   std::cout << "test "<< i << ": "
+    //     << std::get<0>(loaded_tests[i].first) << ", "
+    //     << std::get<1>(loaded_tests[i].first) << ", "
+    //     << std::get<2>(loaded_tests[i].first) << ", "
+    //     << "| "
+    //     << loaded_tests[i].second
+    //     << std::endl;
+    // }
   }
 }
