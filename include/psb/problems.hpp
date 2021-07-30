cd8e70602b0e4b8b9bd17b53078ee887f3bd41ae
Alex Lalejini <amlalejini@gmail.com>
2021-07-30 14:22:47 -0400

    add bowling stub

--- include/psb/problems.hpp
+++ include/psb/problems.hpp
@@ -11,8 +11,6 @@
 
 namespace psb {
 
-
-
 template<typename INPUT_T, typename OUTPUT_T>
 struct ProblemBase {
 
@@ -20,26 +18,35 @@ struct ProblemBase {
   using output_t = OUTPUT_T;
   using json = nlohmann::json;
 
-  using fun_interpret_json_t = std::function< std::pair<input_t,output_t>(const nlohmann::json &) >;
+  using fun_interpret_json_t = std::function< std::pair<input_t,output_t>(const json &) >;
 
   /// Interpret JSON object into input/output pair.
   /// Base implementation naively assumes a single-input, single-output test.
+  /// @NOTE: this might need to get changed to be more generic down the line...
   fun_interpret_json_t fun_interpret_json =
-    [](const nlohmann::json & j) {
-      // return {j["input1"], j["output1"]};
+    [](const json & j) {
       return std::make_pair(j["input1"], j["output1"]);
     };
 
   virtual std::string name() const = 0;
-  virtual std::pair<input_t,output_t> ReadTestJSON(const std::string & s) const = 0;
-  virtual std::pair<input_t,output_t> ReadTestJSON(const json & j) const = 0;
 
-  void SetInterpretJSON(const fun_interpret_json_t & interpret_fun) {
-    fun_interpret_json = interpret_fun;
+  /// Interpet a single test stored as a string but following JSON.
+  virtual std::pair<input_t,output_t> ReadTestJSON(const std::string & s) const {
+    return ReadTestJSON(json::parse(s));
+  }
+
+  /// Interpret a single test stored as a JSON object.
+  virtual std::pair<input_t,output_t> ReadTestJSON(const json & j) const {
+    return fun_interpret_json(j);
+  }
+
+  virtual void SetInterpretJSON(const fun_interpret_json_t & f) {
+    fun_interpret_json = f;
   }
 
 };
 
+// basement
 struct Basement :
   ProblemBase<
     /*INPUT_T=*/emp::vector<int>,
@@ -47,20 +54,67 @@ struct Basement :
   >
 {
 
-  /// Interpet a single test stored as a string but following JSON.
-  std::pair<input_t,output_t> ReadTestJSON(const std::string & test_json_str) const override {
-      return ReadTestJSON(json::parse(test_json_str));
-  }
-
-  /// Interpret a single test stored as a JSON object.
-  std::pair<input_t,output_t> ReadTestJSON(const json & test_json) const override {
-    return fun_interpret_json(test_json);
-  }
-
-  std::string name() const override { return "Basement"; }
+  std::string name() const override { return "basement"; }
 
 };
 
+// bouncing-balls
+struct BouncingBalls :
+  ProblemBase<
+    /*INPUT_T=*/std::tuple<double,double,int>,
+    /*OUTPUT_T=*/double
+  >
+{
+
+  BouncingBalls() {
+    SetInterpretJSON(
+      [](const json & j) {
+        return std::make_pair(
+          std::make_tuple(j["input1"], j["input2"], j["input3"]),
+          j["output1"]
+        );
+      }
+    );
+  }
+
+  std::string name() const override { return "bouncing-balls"; }
+
+};
+
+// bowling
+struct Bowling :
+  ProblemBase<
+    /*INPUT_T=*/std::string,
+    /*OUTPUT_T=*/int
+  >
+{
+
+  std::string name() const override { return "bowling"; }
+
+};
+
+// camel-case
+// coin-sums
+// cut-vector
+// dice-game
+// find-pair
+// fizz-buzz
+// final-cost
+// greatest-common-divisor
+// indices-of-substring
+// leaders
+// luhn
+// mastermind
+// middle-character
+// paired-digits
+// shopping-list
+// snow-day
+// solve-boolean
+// spin-words
+// square-digits
+// substitution-cipher
+// twitter
+// vector-distance
 
 
 }
