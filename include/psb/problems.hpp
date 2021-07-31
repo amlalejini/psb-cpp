a9e8dae5feb80bc16db2af8dee0e45b0444bf155
Alex Lalejini <amlalejini@gmail.com>
2021-07-31 10:43:20 -0400

    draft more problem definitions

--- include/psb/problems.hpp
+++ include/psb/problems.hpp
@@ -94,27 +94,258 @@ struct Bowling :
 };
 
 // camel-case
+struct CamelCase:
+  ProblemBase<
+    /*INPUT_T=*/std::string,
+    /*OUTPUT_T=*/std::string
+  >
+{
+
+  std::string name() const override { return "camel-case"; }
+
+};
+
 // coin-sums
+struct CoinSums:
+  ProblemBase<
+    /*INPUT_T=*/int,
+    /*OUTPUT_T=*/emp::array<int, 4>
+  >
+{
+  CoinSums() {
+    SetInterpretJSON(
+      [](const json & j) {
+        return std::make_pair(
+          j["input1"],
+          {j["output1"],j["output2"],j["output3"],j["output4"]}
+        );
+      }
+    );
+  }
+
+  std::string name() const override { return "coin-sums"; }
+};
+
 // cut-vector
+struct CutVector:
+  ProblemBase<
+    /*INPUT_T=*/emp::vector<int>,
+    /*OUTPUT_T=*/emp::array<emp::vector<int>,2>
+  >
+{
+  CutVector() {
+    SetInterpretJSON(
+      [](const json & j) {
+        return std::make_pair(
+          j["input1"],
+          {j["output1"],j["output2"]}
+        );
+      }
+    );
+  }
+  std::string name() const override { return "cut-vector"; }
+};
+
 // dice-game
-// find-pair
-// fizz-buzz
-// final-cost
-// greatest-common-divisor
-// indices-of-substring
-// leaders
-// luhn
-// mastermind
-// middle-character
-// paired-digits
-// shopping-list
-// snow-day
-// solve-boolean
-// spin-words
-// square-digits
-// substitution-cipher
-// twitter
-// vector-distance
+struct DiceGame:
+  ProblemBase<
+    /*INPUT_T=*/emp::array<int,2>,
+    /*OUTPUT_T=*/double
+  >
+{
+  CutVector() {
+    SetInterpretJSON(
+      [](const json & j) {
+        return std::make_pair(
+          {j["input1"],j["input2"]},
+          j["output1"]
+        );
+      }
+    );
+  }
+  std::string name() const override { return "dice-game"; }
+};
+
+// // find-pair
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "find-pair"; }
+// };
+
+// // fizz-buzz
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "fizz-buzz"; }
+// };
+
+// // final-cost
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "final-cost"; }
+// };
+
+// // greatest-common-divisor
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "greatest-common-divisor"; }
+// };
+
+// // indices-of-substring
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "indices-of-substring"; }
+// };
+
+// // leaders
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "leaders"; }
+// };
+
+// // luhn
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "luhn"; }
+// };
+
+// // mastermind
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "mastermind"; }
+// };
+
+// // middle-character
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "middle-character"; }
+// };
+
+// // paired-digits
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "paired-digits"; }
+// };
+
+// // shopping-list
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "shopping-list"; }
+// };
+
+// // snow-day
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "snow-day"; }
+// };
+
+// // solve-boolean
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "solve-boolean"; }
+// };
+
+// // spin-words
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "spin-words"; }
+// };
+
+// // square-digits
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "square-digits"; }
+// };
+
+// // substitution-cipher
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "substitution-cipher"; }
+// };
+
+// // twitter
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "twitter"; }
+// };
+
+// // vector-distance
+// struct :
+//   ProblemBase<
+//     /*INPUT_T=*/
+//     /*OUTPUT_T=*/
+//   >
+// {
+//   std::string name() const override { return "vector-distance"; }
+// };
+
 
 
 }
