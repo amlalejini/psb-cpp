d9afb5117894d331ba83b8078c8239d326a7850d
Alexander Lalejini <amlalejini@gmail.com>
2022-12-18 19:38:14 -0500

    add single include for problem readers

---
+++ include/psb/problem_readers.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "readers/Basement.hpp"
+#include "readers/BouncingBalls.hpp"
+#include "readers/Bowling.hpp"
+#include "readers/CamelCase.hpp"
+#include "readers/CoinSums.hpp"
+#include "readers/CutVector.hpp"
+#include "readers/DiceGame.hpp"
