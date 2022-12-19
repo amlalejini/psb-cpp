966610dec3eac3a6d23d22638290821f90c5d90c
Alexander Lalejini <amlalejini@gmail.com>
2022-12-18 19:57:03 -0500

    implement gcd reader

---
+++ include/psb/readers/GCD.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <utility>
+#include <string>
+#include <functional>
+
+#include "json/json.hpp"
+
+#include "emp/base/vector.hpp"
+#include "emp/base/array.hpp"
+
+#include "BaseProblemReader.hpp"
+
+namespace psb {
+
+namespace reader {
+
+// GCD (greatest common divisor)
+struct GCD : public BaseProblemReader<
+  int,
+  int
+> {
+
+  std::string name() const override { return "gcd"; }
+
+};
+
+} // end reader namespace
+
+} // end psb namespace
